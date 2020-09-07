066acb4e16db9d81fe4d1173a039090719a5cf00
bigEagleY <124763198@qq.com>
2020-09-07 11:26:26 +0800

    springboot-异常处理方法二

--- springboot-exception/src/main/java/com/daguo/method1/GlobalExceptionHandler.java
+++ springboot-exception/src/main/java/com/daguo/method1/GlobalExceptionHandler.java
@@ -8,19 +8,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * 新建异常处理类
  * 方法一：使用 @ControllerAdvice和@ExceptionHandler处理全局异常
+ * 在类上加上@ControllerAdvice注解这个类就成为了全局异常处理类，
+ * 当然也可以通过 assignableTypes 指定特定的 Controller 类，让异常处理类只处理特定类抛出的异常。
  */
-@ControllerAdvice()
+@ControllerAdvice/*(assignableTypes = {ExceptionController.class})*/
 @ResponseBody
 public class GlobalExceptionHandler {
+    //用自定义的错误类型：返回指定的错误
     ErrorResponse illegalArgumentResponse = new ErrorResponse(new IllegalArgumentException("参数错误!"));
     ErrorResponse businessResponse = new ErrorResponse(new BusinessException("自定义异常错误"));
 
-    @ExceptionHandler(value = Exception.class)// 拦截所有异常, 这里只是为了演示，一般情况下一个方法特定处理一种异常
+
+    RuntimeException bException  = new BusinessException("自定义异常错误1");// 因为要统一返回类型
+
+    //@ExceptionHandler 处理 Controller 级别的异常
+   /* @ExceptionHandler(value = Exception.class)// 拦截所有异常, 这里只是为了演示，一般情况下一个方法特定处理一种异常
     public ResponseEntity<ErrorResponse> exceptionHandler(Exception e){
         if(e instanceof IllegalArgumentException){
             return ResponseEntity.status(400).body(illegalArgumentResponse);
         }else if(e instanceof BusinessException){
             return ResponseEntity.status(404).body(businessResponse);
+        }
+        return null;
+    }*/
+
+
+    @ExceptionHandler(value = Exception.class)// 拦截所有异常, 这里只是为了演示，一般情况下一个方法特定处理一种异常
+    public ResponseEntity<RuntimeException> exceptionHandler1(Exception e){
+        if(e instanceof BusinessException){
+            return ResponseEntity.status(404).body(bException);
         }
         return null;
     }
--- springboot-exception/src/test/java/ExceptionTest.java
+++ springboot-exception/src/test/java/ExceptionTest.java
@@ -1,3 +1,33 @@
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ExceptionTest {
+
+    @Autowired
+    MockMvc mockMvc;//实现了对Http请求的模拟
+
+    @Test
+    public void should_return_400_if_param_not_valid() throws Exception {
+        mockMvc.perform(get("/illegalArgumentException"))
+                .andExpect(status().is(400))
+                .andExpect(jsonPath("$.message").value("参数错误!"));
+    }
+
+    @Test
+    public void should_return_404_if_business_found() throws Exception {
+        mockMvc.perform(get("/businessException"))
+                .andExpect(status().is(404))
+                .andExpect(jsonPath("$.message").value("自定义异常错误!"));
+    }
+
 }
