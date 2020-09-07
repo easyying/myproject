d5ef403b94c9a8a8a0e378d8624440c1e6c6e754
bigEagleY <124763198@qq.com>
2020-09-07 18:33:42 +0800

    springboot过滤器-过滤器的原理和作用

--- springboot-filter/src/main/java/com/daguo/controller/MyController.java
+++ springboot-filter/src/main/java/com/daguo/controller/MyController.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author
- * 验证过滤器
+ * 验证过滤器：
+ * 原理：Filter 过滤器是面向切面编程——AOP 的具体实现
+ * 作用：过滤用户请求的，它允许我们对用户请求进行前置处理和后置处理：比如实现URL级别的权限控制、过滤非法请求等等
  *
  * 1. 定义过滤器MyFilter：要实现Filter接口
  * 2. 定义配置MyFilterConfig
  * 3. 在配置中注册自定义的过滤器
  *    手动配置 @@Autowired private MyFilter myFilter;
  *    注解实现（还未跑通）
- *
- *
  */
 @RestController
 @RequestMapping("/filtercontro")
